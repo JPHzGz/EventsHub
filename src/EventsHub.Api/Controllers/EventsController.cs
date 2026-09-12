3f02892f4db4b5190d87c03daa79b742f8f87536
JPHzGz <101302963+JPHzGz@users.noreply.github.com>
2026-09-12 01:08:46 -0600

    Parcial01 - Add API skeleton
    * feat(parcial01) - add initial boilerplate
    
    * feat(parcial01) - cleanup
    
    * feat(parcial01) - added entity and EF boilerplate
    
    * Fix 'Activity' name
    
    * feat(parcial01) - added first entity and initial migration
    
    * feat(parcial01) - added db initializer
    
    * feat(parcial01) - renamed repo
    
    * feat(parcial01) - added base and events controller
    
    * feat(parcial01) - added bruno integration tests
    
    * feat(parcial01) - updated bruno integration tests
    
    * feat(parcial01) - added openapi including rpc client generation

---
+++ src/EventsHub.Api/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+using EventsHub.Domain;
+using EventsHub.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventsHub.Api.Controllers
+{
+    public class EventsController(AppDbContext context) : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Event>>> GetEventsAsynv()
+        {
+            return await context.Events.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Event>> GetEventByIdAsync(string id)
+        {
+            var result = await context.Events.FindAsync(id);
+            if (result == null)
+            {
+                return NotFound("The event was not found");
+            }
+            return result;
+        }
+    }
+}
---
+++ src/EventsHub.Persistence/AppDbContext.cs
@@ -0,0 +1,10 @@
+using EventsHub.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventsHub.Persistence
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Event> Events { get; set; }
+    }
+}
