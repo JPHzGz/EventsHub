36671f9f31df735fcd2ef7881ed069a2bf31f8fa
JPHzGz <juan.pablojphg58@gmail.com>
2026-09-13 19:27:37 -0600

    feat(parcial02) - added activity type

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     fetch("https://localhost:5001/api/v1/events")
@@ -17,7 +17,7 @@ function App() {
         Events Hub
       </h3>
       <ul>
-        {activities.map((activity) => (
+        {activities.map((activity: Activity) => (
           <li key={activity.id}>{activity.title}</li>
         ))}
       </ul>
